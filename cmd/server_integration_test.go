54aa3aa869f619c01e03c494ceec4faa267ae6eb
Alnoor Altegany <alnoor.altegany@gmail.com>
2024-06-20 15:44:56 +0300

    test(server): refactor integration tests. add util funcs

--- cmd/server_integration_test.go
+++ cmd/server_integration_test.go
@@ -11,65 +11,69 @@ import (
 )
 
 func TestTasksServer(t *testing.T) {
+	serve := server.NewTasksServer()
 	t.Run("post task and return task element", func(t *testing.T) {
-		task := todo.Task{
-			Description: "هذه مهمة من فحص",
-		}
-		input := strings.NewReader(fmt.Sprintf("description=%s", task.Description))
-		serve := server.NewTasksServer()
+		task, err, response := PostNewTask(serve)
 
-		request, err := http.NewRequest(http.MethodPost, "/tasks", input)
-		// NOTE: setting the header is required
-		request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-		response := httptest.NewRecorder()
-
-		serve.Router.ServeHTTP(response, request)
-
-		if response.Code != http.StatusCreated {
-			t.Errorf("request /tasks failed: response not created, response code: %v", response.Code)
-		}
+		AssertResponseCode(response, http.StatusCreated, t)
 
 		if err != nil {
 			t.Errorf("error in post task: %v", err)
 		}
 
-		if !strings.Contains(response.Body.String(), task.Description) {
-			t.Errorf("got %v wanted %v", response.Body.String(), task.Description)
-		}
+		AssertResponseContain(response, task.Description, t)
 	})
 	t.Run("list tasks when open index", func(t *testing.T) {
-		task := todo.Task{
-			Description: "هذه مهمة من فحص",
-		}
-		input := strings.NewReader(fmt.Sprintf("description=%s", task.Description))
-		serve := server.NewTasksServer()
+		task, err, _ := PostNewTask(serve)
 
-		post_request, err := http.NewRequest(http.MethodPost, "/tasks", input)
-		// NOTE: setting the header is required
-		post_request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		err, response := GetTasksList(serve)
 
-		post_response := httptest.NewRecorder()
-
-		serve.Router.ServeHTTP(post_response, post_request)
-
-		get_request, err := http.NewRequest(http.MethodGet, "/", nil)
-
-		get_response := httptest.NewRecorder()
-
-		serve.Router.ServeHTTP(get_response, get_request)
-
-		if get_response.Code != http.StatusOK {
-			t.Errorf("request / failed: response not created, response code: %v", get_response.Code)
-		}
+		AssertResponseCode(response, http.StatusOK, t)
 
 		if err != nil {
 			t.Errorf("error in post task: %v", err)
 		}
 
-		// if !strings.Contains(response.Body.String(), task.Description) {
-		// 	t.Errorf("got %v wanted %v", response.Body.String(), task.Description)
-		// }
+		AssertResponseContain(response, task.Description, t)
+
 	})
 
+}
+
+func GetTasksList(serve server.Server) (error, *httptest.ResponseRecorder) {
+	get_request, err := http.NewRequest(http.MethodGet, "/", nil)
+
+	get_response := httptest.NewRecorder()
+
+	serve.Router.ServeHTTP(get_response, get_request)
+	return err, get_response
+}
+
+func AssertResponseContain(response *httptest.ResponseRecorder, wanted string, t *testing.T) {
+	if !strings.Contains(response.Body.String(), wanted) {
+		t.Errorf("got %v wanted %v", response.Body.String(), wanted)
+	}
+}
+
+func AssertResponseCode(response *httptest.ResponseRecorder, wanted int, t *testing.T) {
+	if response.Code != wanted {
+		t.Errorf("request /tasks failed: response not created, response code: %v", response.Code)
+	}
+}
+
+func PostNewTask(serve server.Server) (todo.Task, error, *httptest.ResponseRecorder) {
+	task := todo.Task{
+		Description: "هذه مهمة من فحص",
+	}
+	input := strings.NewReader(fmt.Sprintf("description=%s", task.Description))
+
+	request, err := http.NewRequest(http.MethodPost, "/tasks", input)
+
+	// NOTE: setting the header is required
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	response := httptest.NewRecorder()
+
+	serve.Router.ServeHTTP(response, request)
+	return task, err, response
 }
