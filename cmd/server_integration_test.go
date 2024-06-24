85fe0faedaeeec213e1551e36e18f014a68c28cf
noor-tg <nour.dev.00@gmail.com>
2024-06-24 11:04:35 +0300

    test(server): extract test utils funcs to file. update toggle status test

--- cmd/server_integration_test.go
+++ cmd/server_integration_test.go
@@ -1,14 +1,9 @@
 package main_test
 
 import (
-	"alnoor/todo-go-htmx"
 	"alnoor/todo-go-htmx/server"
 	"fmt"
 	"net/http"
-	"net/http/httptest"
-	"regexp"
-	"strconv"
-	"strings"
 	"testing"
 )
 
@@ -66,6 +61,19 @@ func TestTasksServer(t *testing.T) {
 		AssertResponseContain(response, task.Description, t)
 	})
 
+	t.Run("send request to /tasks/toggle-status/id return task toggled", func(t *testing.T) {
+		task, err, response := PostNewTask(serve)
+		task, response, err = ToggleTaskStatus(task, serve)
+
+		AssertResponseCode(response, http.StatusOK, t)
+
+		if err != nil {
+			t.Errorf("error in post task: %v", err)
+		}
+
+		AssertResponseContain(response, `checked="checked"`, t)
+	})
+
 	t.Run("submit delete request to /tasks/id should delete task", func(t *testing.T) {
 		task, err, response := PostNewTask(serve)
 		response, err = DeleteTask(task, serve)
@@ -76,101 +84,4 @@ func TestTasksServer(t *testing.T) {
 			t.Errorf("error in post task: %v", err)
 		}
 	})
-}
-
-func GetTasksList(serve server.Server) (error, *httptest.ResponseRecorder) {
-	request, err := http.NewRequest(http.MethodGet, "/", nil)
-
-	response := httptest.NewRecorder()
-
-	serve.Router.ServeHTTP(response, request)
-	return err, response
-}
-
-func AssertResponseContain(response *httptest.ResponseRecorder, wanted string, t testing.TB) {
-	t.Helper()
-	if !strings.Contains(response.Body.String(), wanted) {
-		t.Errorf("got %v wanted %v", response.Body.String(), wanted)
-	}
-}
-
-func AssertResponseCode(response *httptest.ResponseRecorder, wanted int, t testing.TB) {
-	t.Helper()
-
-	if response.Code != wanted {
-		t.Errorf("request /tasks failed: response not created, response code: %v", response.Code)
-	}
-}
-
-func PostNewTask(serve server.Server) (todo.Task, error, *httptest.ResponseRecorder) {
-	task := todo.Task{
-		Description: "هذه مهمة من فحص",
-	}
-	input := strings.NewReader(fmt.Sprintf("description=%s", task.Description))
-
-	request, err := http.NewRequest(http.MethodPost, "/tasks", input)
-
-	// NOTE: setting the header is required
-	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	response := httptest.NewRecorder()
-
-	serve.Router.ServeHTTP(response, request)
-
-	task.Id = GetTaskId(response)
-
-	return task, err, response
-}
-
-func PutTask(task todo.Task, serve server.Server) (todo.Task, *httptest.ResponseRecorder, error) {
-	input := strings.NewReader(fmt.Sprintf("description=%s", task.Description))
-	request, err := http.NewRequest(http.MethodPut, fmt.Sprintf("/tasks/%d", task.Id), input)
-
-	// NOTE: setting the header is required
-	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	response := httptest.NewRecorder()
-
-	serve.Router.ServeHTTP(response, request)
-
-	task.Id = GetTaskId(response)
-
-	return task, response, err
-}
-
-func DeleteTask(task todo.Task, serve server.Server) (*httptest.ResponseRecorder, error) {
-	request, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("/tasks/%d", task.Id), nil)
-
-	// NOTE: setting the header is required
-	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	response := httptest.NewRecorder()
-
-	serve.Router.ServeHTTP(response, request)
-
-	task.Id = GetTaskId(response)
-
-	return response, err
-}
-
-func GetTaskId(response *httptest.ResponseRecorder) int {
-	pattern := regexp.MustCompile(`hx-(put|post|get|delete)="/tasks/(\d+)"`)
-
-	body := response.Body.String()
-
-	matches := pattern.FindStringSubmatch(body)
-	if len(matches) > 0 {
-		id_num, _ := strconv.Atoi(matches[2])
-		return id_num
-	}
-
-	return 0
-}
-
-func GetUpdateTaskForm(task todo.Task, serve server.Server) (*httptest.ResponseRecorder, error) {
-	request, err := http.NewRequest(http.MethodGet, fmt.Sprintf("/tasks/%d", task.Id), nil)
-	response := httptest.NewRecorder()
-
-	serve.Router.ServeHTTP(response, request)
-	return response, err
 }
---
+++ cmd/server_integration_utils_test.go
@@ -0,0 +1,125 @@
+package main_test
+
+import (
+	"alnoor/todo-go-htmx"
+	"alnoor/todo-go-htmx/server"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func GetTasksList(serve server.Server) (error, *httptest.ResponseRecorder) {
+	request, err := http.NewRequest(http.MethodGet, "/", nil)
+
+	response := httptest.NewRecorder()
+
+	serve.Router.ServeHTTP(response, request)
+	return err, response
+}
+
+func AssertResponseContain(response *httptest.ResponseRecorder, wanted string, t testing.TB) {
+	t.Helper()
+	if !strings.Contains(response.Body.String(), wanted) {
+		t.Errorf("got %v wanted %v", response.Body.String(), wanted)
+	}
+}
+
+func AssertResponseCode(response *httptest.ResponseRecorder, wanted int, t testing.TB) {
+	t.Helper()
+
+	if response.Code != wanted {
+		t.Errorf("request /tasks failed: response not created, response code: %v", response.Code)
+	}
+}
+
+func PostNewTask(serve server.Server) (todo.Task, error, *httptest.ResponseRecorder) {
+	task := todo.Task{
+		Description: "هذه مهمة من فحص",
+	}
+	input := strings.NewReader(fmt.Sprintf("description=%s", task.Description))
+
+	request, err := http.NewRequest(http.MethodPost, "/tasks", input)
+
+	// NOTE: setting the header is required
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	response := httptest.NewRecorder()
+
+	serve.Router.ServeHTTP(response, request)
+
+	task.Id = GetTaskId(response)
+
+	return task, err, response
+}
+
+func PutTask(task todo.Task, serve server.Server) (todo.Task, *httptest.ResponseRecorder, error) {
+	input := strings.NewReader(fmt.Sprintf("description=%s", task.Description))
+	request, err := http.NewRequest(http.MethodPut, fmt.Sprintf("/tasks/%d", task.Id), input)
+
+	// NOTE: setting the header is required
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	response := httptest.NewRecorder()
+
+	serve.Router.ServeHTTP(response, request)
+
+	task.Id = GetTaskId(response)
+
+	return task, response, err
+}
+
+func ToggleTaskStatus(task todo.Task, serve server.Server) (todo.Task, *httptest.ResponseRecorder, error) {
+	request, err := http.NewRequest(http.MethodPut, fmt.Sprintf("/tasks/toggle-status/%d", task.Id), nil)
+
+	// NOTE: setting the header is required
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	response := httptest.NewRecorder()
+
+	serve.Router.ServeHTTP(response, request)
+
+	task.Id = GetTaskId(response)
+
+	return task, response, err
+}
+
+func DeleteTask(task todo.Task, serve server.Server) (*httptest.ResponseRecorder, error) {
+	request, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("/tasks/%d", task.Id), nil)
+
+	// NOTE: setting the header is required
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	response := httptest.NewRecorder()
+
+	serve.Router.ServeHTTP(response, request)
+
+	task.Id = GetTaskId(response)
+
+	return response, err
+}
+
+func GetTaskId(response *httptest.ResponseRecorder) int {
+	pattern := regexp.MustCompile(`hx-(put|post|get|delete)="/tasks/(\d+)"`)
+
+	body := response.Body.String()
+
+	matches := pattern.FindStringSubmatch(body)
+	if len(matches) > 0 {
+		id_num, _ := strconv.Atoi(matches[2])
+		return id_num
+	}
+
+	return 0
+}
+
+func GetUpdateTaskForm(task todo.Task, serve server.Server) (*httptest.ResponseRecorder, error) {
+	request, err := http.NewRequest(http.MethodGet, fmt.Sprintf("/tasks/%d", task.Id), nil)
+	response := httptest.NewRecorder()
+
+	serve.Router.ServeHTTP(response, request)
+	return response, err
+}
