09c57c61dfe3aa34c31e12118e8a8b86503e4c6e
noor-tg <nour.dev.00@gmail.com>
2024-07-01 22:06:48 +0300

    fix(counters): hide all label when completed = all. fix label position

--- static/animate-progress.js
+++ static/animate-progress.js
@@ -2,10 +2,7 @@ function animateProgressBar() {
   // elements
   let progressBar = document.querySelector("progress");
   let completedCounter = document.querySelector("#completed-counter");
-  let countElement;
-  if (completedCounter != null) {
-    countElement = completedCounter.children.item(1);
-  }
+  let countElement = completedCounter.children.item(0);
   // original to animate to
   let original = parseFloat(progressBar.getAttribute("data-value"));
   // max to calculate completed persentage of current value
@@ -14,27 +11,25 @@ function animateProgressBar() {
   let old = parseFloat(progressBar.getAttribute("data-old-completed"));
   // calculate position right based on multiple values
   let right = (value) => {
-    counterWidth = 0.04 * progressBar.clientWidth;
+    let val = window.innerWidth < 400 ? 0.07 : 0.04;
+    counterWidth = val * progressBar.clientWidth;
     // convert from persentage to value in pixel using progressbar width
     return (parseFloat(value) / max) * progressBar.clientWidth - counterWidth;
   };
   // set value to old value to start animation
   // -- set current to old
   progressBar.value = old;
-  if (completedCounter != null) {
-    // show count label
-    completedCounter.style.display = `flex`;
-    // set starting right position to calculated right
-    completedCounter.style.right = `${right(progressBar.value)}px`;
-    countElement.innerText = Math.ceil(progressBar.value);
-  }
+  // show count label
+  completedCounter.style.display = `flex`;
+  // set starting right position to calculated right
+  completedCounter.style.right = `${right(progressBar.value)}px`;
+  countElement.innerText = Math.ceil(progressBar.value);
   // animation timer
   let timer = 5;
   function updateProgress() {
-    if (completedCounter != null) {
-      completedCounter.style.right = `${right(progressBar.value)}px`;
-      countElement.innerText = Number(progressBar.value).toFixed(0);
-    }
+    completedCounter.style.right = `${right(progressBar.value)}px`;
+    countElement.innerText = Number(progressBar.value).toFixed(0);
+
     if (original < old) {
       if (progressBar.value > original) {
         progressBar.value -= 0.02;
